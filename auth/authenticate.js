e81da149f6bc87427932a6d0b77377187979a5fc
croy <roy.christie95@gmail.com>
2019-03-01 10:15:14 -0600

    Added dotENV file with JWT secret variable, updated variable

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const jwtKey =
   process.env.JWT_SECRET ||
-  'add a .env file to root of project with the JWT_SECRET variable';
+  'I watched a beaver movie yesterday. Best dam movie I ever saw.';
 
 // quickly see what this file exports
 module.exports = {
